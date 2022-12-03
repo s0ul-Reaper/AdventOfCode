9d1049b7ff16928afa376974cc8b1cc1e384ec97
ioannis-bekiaris <giannismpek247@gmail.com>
2022-12-03 15:10:07 +0200

    fixing dir, beginning 2022 journey

--- 2021/Day1/day1.py
+++ 2021/Day1/day1.py
@@ -49,4 +49,4 @@ result1 = sum_of_incr(numbers)
 result2 = sum_of_incr(add_per_three(numbers))
 
 print(
-    f'The result of part one is: {result1}\nThe result of part two is: {result2} ')
+    f'The result of part one is: {result1}\nThe result of part two is:/n {result2} ')
---
+++ 2022/day1/day1.py
@@ -0,0 +1,24 @@
+with open('input.txt', 'r') as f:
+    temp = []
+    for line in f:
+        temp.append(line)
+
+temp = [l.strip('\n') for l in temp]
+
+
+c = 0
+final = []
+
+for i in range(len(temp)):
+    if temp[i] != '':
+        c += int(temp[i])
+    else:
+        final.append(c)
+        c = 0
+
+# 68293, 68501, 69310]
+
+final = sorted(final)[-3::]
+
+print(
+    f'The answer of part 1 is: {max(final)}\nThe answer of part 2 is: {sum(final)}')
