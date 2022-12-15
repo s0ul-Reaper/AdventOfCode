d6e51c8529777c4a5b432553ad85239c2d4574e7
ioannis-bekiaris <giannismpek247@gmail.com>
2022-12-15 23:48:18 +0200

    no else statement

--- 2022/Day6/day6.py
+++ 2022/Day6/day6.py
@@ -5,9 +5,7 @@ with open('input.txt', 'r') as f:
 def det(x, y):
     for i in range(len(x)):
         temp = x[i:i+y]
-        if len(set(temp)) != len(temp):
-            continue
-        else:
+        if len(set(temp)) == len(temp):
             return i+y
 
 
@@ -17,7 +15,7 @@ print(
 
 ################ DONK SOLUTION ################
 
-# def detective(x: str, y: int) -> bool:
+# def detective(x: str, y: int) -> int:
 #     for i in range(len(x)):
 #         if x[i:i+y].count(x[i]) == 1 and x[i:i+y].count(x[i+1]) == 1 and x[i:i+y].count(x[i+2]) == 1 and x[i:i+y].count(x[i+3]) == 1:
 #             return i+y
@@ -25,7 +23,7 @@ print(
 #             continue
 
 
-# def detective2(x: str, y: int) -> bool:
+# def detective2(x: str, y: int) -> int:
 #     for i in range(len(x)):
 #         if x[i:i+y].count(x[i]) == 1 and x[i:i+y].count(x[i+1]) == 1 and x[i:i+y].count(x[i+2]) == 1 and x[i:i+y].count(x[i+3]) == 1\
 #             and x[i:i+y].count(x[i+y]) == 1 and x[i:i+y].count(x[i+5]) == 1 and x[i:i+y].count(x[i+6]) == 1 and x[i:i+y].count(x[i+7]) == 1\
