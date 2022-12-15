fb5c7a0036e093a69cc0ab67560b1b5fd5b0d85a
ioannis-bekiaris <giannismpek247@gmail.com>
2022-12-15 23:20:04 +0200

    Day 6

--- 2022/Day5/day5.py
+++ 2022/Day5/day5.py
@@ -1,7 +1,7 @@
 import numpy as np
-with open('input.txt', 'r') as f:
+with open('temp.txt', 'r') as f:
     input = [line.replace('\n', '').replace('move ', '').replace(
-        ' from ', ',').replace(' to ', ',').split(',') for line in f][10::]
+        ' from ', ',').replace(' to ', ',').split(',') for line in f][5::]
 
 # input.txt, [10::]
 
@@ -10,35 +10,36 @@ def str_to_int(x: str) -> list:
     return [int(i) for i in x]
 
 
-temp = [str_to_int(i) for i in input]
 des = [str_to_int(i) for i in input]
+
+temp = [str_to_int(i) for i in input]
 
 for i in temp:
     i[1] = i[1]-1
 data = temp
 
 # # Matix initialization
-matrix = np.empty((56, 9), dtype=str)
+# matrix = np.empty((56, 9), dtype=str)
 
-matrix[48][1], matrix[48][4], matrix[48][6] = 'W', 'J', 'J'
-matrix[49][1], matrix[49][3], matrix[49][4], matrix[49][5], matrix[49][6] = 'V', 'F', 'F', 'S', 'S'
-matrix[50][1], matrix[50][2], matrix[50][3], matrix[50][4], matrix[50][5], matrix[50][6] = 'S', 'M', 'R', 'W', 'M', 'C'
-matrix[51][1], matrix[51][2], matrix[51][3], matrix[51][4], matrix[51][5], matrix[51][6], matrix[51][8] = 'M', 'G', 'W', 'S', 'F', 'G', 'C'
-matrix[52][0], matrix[52][1], matrix[52][2], matrix[52][3], matrix[52][4], matrix[
-    52][5], matrix[52][6], matrix[52][8] = 'W', 'P', 'S', 'M', 'H', 'N', 'F', 'L'
-matrix[53][0], matrix[53][1], matrix[53][2], matrix[53][3], matrix[53][4], matrix[53][5], matrix[
-    53][6], matrix[53][7], matrix[53][8] = 'R', 'H', 'T', 'D', 'L', 'D', 'D', 'B', 'W'
-matrix[54][0], matrix[54][1], matrix[54][2], matrix[54][3], matrix[54][4], matrix[54][5], matrix[
-    54][6], matrix[54][7], matrix[54][8] = 'T', 'C', 'L', 'H', 'Q', 'J', 'B', 'T', 'N'
-matrix[55][0], matrix[55][1], matrix[55][2], matrix[55][3], matrix[55][4], matrix[55][5], matrix[
-    55][6], matrix[55][7], matrix[55][8] = 'G', 'G', 'C', 'J', 'P', 'P', 'Z', 'R', 'H'
+# matrix[48][1], matrix[48][4], matrix[48][6] = 'W', 'J', 'J'
+# matrix[49][1], matrix[49][3], matrix[49][4], matrix[49][5], matrix[49][6] = 'V', 'F', 'F', 'S', 'S'
+# matrix[50][1], matrix[50][2], matrix[50][3], matrix[50][4], matrix[50][5], matrix[50][6] = 'S', 'M', 'R', 'W', 'M', 'C'
+# matrix[51][1], matrix[51][2], matrix[51][3], matrix[51][4], matrix[51][5], matrix[51][6], matrix[51][8] = 'M', 'G', 'W', 'S', 'F', 'G', 'C'
+# matrix[52][0], matrix[52][1], matrix[52][2], matrix[52][3], matrix[52][4], matrix[
+#     52][5], matrix[52][6], matrix[52][8] = 'W', 'P', 'S', 'M', 'H', 'N', 'F', 'L'
+# matrix[53][0], matrix[53][1], matrix[53][2], matrix[53][3], matrix[53][4], matrix[53][5], matrix[
+#     53][6], matrix[53][7], matrix[53][8] = 'R', 'H', 'T', 'D', 'L', 'D', 'D', 'B', 'W'
+# matrix[54][0], matrix[54][1], matrix[54][2], matrix[54][3], matrix[54][4], matrix[54][5], matrix[
+#     54][6], matrix[54][7], matrix[54][8] = 'T', 'C', 'L', 'H', 'Q', 'J', 'B', 'T', 'N'
+# matrix[55][0], matrix[55][1], matrix[55][2], matrix[55][3], matrix[55][4], matrix[55][5], matrix[
+#     55][6], matrix[55][7], matrix[55][8] = 'G', 'G', 'C', 'J', 'P', 'P', 'Z', 'R', 'H'
 
 
-# mat = np.empty((6, 3), dtype=str)
+matrix = np.empty((6, 3), dtype=str)
 
-# mat[5][0], mat[5][1], mat[5][2] = 'Z', 'M', 'P'
-# mat[4][0], mat[4][1] = 'N', 'C'
-# mat[3][1] = 'D'
+matrix[5][0], matrix[5][1], matrix[5][2] = 'Z', 'M', 'P'
+matrix[4][0], matrix[4][1] = 'N', 'C'
+matrix[3][1] = 'D'
 
 # mat[5][1], mat[5, 2] = 'M', 'P'
 # mat[4][1], mat[4, 2] = 'C', 'D'
@@ -72,13 +73,18 @@ def destination(matrix: np.ndarray, x: list) -> list:
             break
 
 
-for y in range(len(data)):
-    #print(f'Y IS {y}')
-    for i in range(data[y][0]):
-        #print(f'Looping for the {i} time')
-        matrix[destination(matrix, des[y])[0]][destination(matrix, des[y])
-                                               [1]] = hill_point(matrix, data[y][1])[0]
-        matrix[hill_point(matrix, data[y][1])[1][0]][hill_point(
-            matrix, data[y][1])[1][1]] = ''
+# for y in range(len(data)):
+#     #print(f'Y IS {y}')
+#     for i in range(data[y][0]):
+#         #print(f'Looping for the {i} time')
+#         matrix[destination(matrix, des[y])[0]][destination(matrix, des[y])
+#                                                [1]] = hill_point(matrix, data[y][1])[0]
+#         matrix[hill_point(matrix, data[y][1])[1][0]][hill_point(
+#             matrix, data[y][1])[1][1]] = ''
 
 print(matrix)
+
+print(des)
+
+
+print(hill_point(matrix, data[2][0]))
---
+++ 2022/Day6/day6.py
@@ -0,0 +1,36 @@
+with open('input.txt', 'r') as f:
+    data = f.read()
+
+
+def det(x, y):
+    for i in range(len(x)):
+        temp = x[i:i+y]
+        if len(set(temp)) != len(temp):
+            continue
+        else:
+            return i+y
+
+
+print(
+    f'The solution of part a is {det(data, 4)}\nThe solution of part b is {det(data, 14)}')
+
+
+################ DONK SOLUTION ################
+
+# def detective(x: str, y: int) -> bool:
+#     for i in range(len(x)):
+#         if x[i:i+y].count(x[i]) == 1 and x[i:i+y].count(x[i+1]) == 1 and x[i:i+y].count(x[i+2]) == 1 and x[i:i+y].count(x[i+3]) == 1:
+#             return i+y
+#         else:
+#             continue
+
+
+# def detective2(x: str, y: int) -> bool:
+#     for i in range(len(x)):
+#         if x[i:i+y].count(x[i]) == 1 and x[i:i+y].count(x[i+1]) == 1 and x[i:i+y].count(x[i+2]) == 1 and x[i:i+y].count(x[i+3]) == 1\
+#             and x[i:i+y].count(x[i+y]) == 1 and x[i:i+y].count(x[i+5]) == 1 and x[i:i+y].count(x[i+6]) == 1 and x[i:i+y].count(x[i+7]) == 1\
+#             and x[i:i+y].count(x[i+8]) == 1 and x[i:i+y].count(x[i+9]) == 1 and x[i:i+y].count(x[i+10]) == 1 and x[i:i+y].count(x[i+11]) == 1\
+#                 and x[i:i+y].count(x[i+12]) == 1 and x[i:i+y].count(x[i+13]):
+#             return i+y
+#         else:
+#             continue
